1bd8342e59c0984b8d6272db55a3a15eab46e470
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-01-07 23:06:38 +0530

    Update general.cpp

--- Bit Manipulation/general.cpp
+++ Bit Manipulation/general.cpp
@@ -1,16 +1,17 @@
-ll setBit(ll n, ll k)
+// Function to set the kth bit of n
+int setBit(int n, int k)
 {
     return (n | (1 << (k - 1)));
 }
   
 // Function to clear the kth bit of n
-ll clearBit(ll n, ll k)
+int clearBit(int n, int k)
 {
     return (n & (~(1 << (k - 1))));
 }
   
 // Function to toggle the kth bit of n
-ll toggleBit(ll n, ll k)
+int toggleBit(int n, int k)
 {
     return (n ^ (1 << (k - 1)));
 }
