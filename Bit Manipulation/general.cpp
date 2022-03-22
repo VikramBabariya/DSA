518930196d386a3a61d2cb919d9fe4b7d28ee1b4
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-03-22 22:14:58 +0530

    added  Function to check kth bit is set or not

--- Bit Manipulation/general.cpp
+++ Bit Manipulation/general.cpp
@@ -15,3 +15,19 @@ int toggleBit(int n, int k)
 {
     return (n ^ (1 << (k - 1)));
 }
+
+// Function to check kth bit is set or not 
+//LSB is 1
+bool isSet(int n, int k)
+{
+    if (n & (1 << (k - 1)))
+        return true;
+    return false;
+}
+
+int main(){
+
+    // test    
+    
+    return 0;
+}
