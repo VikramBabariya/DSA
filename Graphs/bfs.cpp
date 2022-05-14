1927a6c6a06afa21c5535f4a7c9bd856507c74c1
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-05-14 19:11:49 +0530

    Create bfs.cpp

---
+++ Graphs/bfs.cpp
@@ -0,0 +1,28 @@
+vector<int> bfs(vector<vector<int>> adj, int n) {
+	vector<int> bfsTrv;
+	vector<bool> vis(n + 1, false);
+
+	for (int i = 1; i <= n; i++) {
+		if (vis[i] == false) {
+
+			queue<int> nodes;
+			nodes.push(i);
+			vis[i] = true;
+
+			while (!nodes.empty()) {
+				int node = nodes.front();
+				nodes.pop();
+				bfsTrv.push_back(node);
+
+				for (auto adjNode : adj[node]) {
+					if (vis[adjNode] == false) {
+						nodes.push(adjNode);
+						vis[adjNode] = true;
+					}
+				}
+			}
+
+		}
+	}
+	return bfsTrv;
+}
