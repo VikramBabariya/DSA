04d692953fff63f046f3d3b13aa40311cf03cb44
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-05-14 19:41:37 +0530

    added recursive method for dfs

--- Graphs/dfs.cpp
+++ Graphs/dfs.cpp
@@ -31,4 +31,26 @@ vector<int> dfs(vector<vector<int>> adj, int n) {
 
 
 // method 2 using recursion
+void dfsHelp(int node, vector<vector<int>> &adj, vector<bool> &vis, vector<int> &dfsTrv) {
+	if (vis[node] == true) return;
+	vis[node] = true;
+	dfsTrv.push_back(node);
 
+	for (auto adjNode : adj[node]) {
+		if (vis[adjNode] == false) {
+			dfsHelp(adjNode, adj, vis, dfsTrv);
+		}
+	}
+}
+
+vector<int> dfs(vector<vector<int>> adj, int n) {
+	vector<int> dfsTrv;
+	vector<bool> vis(n + 1, false);
+
+	for (int i = 1; i <= n; i++) {
+		if (vis[i] == false) {
+			dfsHelp(i, adj, vis, dfsTrv);
+		}
+	}
+	return dfsTrv;
+}
