c42b6d0d6242f4b49aca6016bf9112f9c94f8a6e
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-06-17 17:35:29 +0530

    Create Search in Rotated Sorted Array.cpp

---
+++ BinarySearch/SolvedProblems/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    
+    int findPivot(vector<int>& arr){
+        int n = arr.size(), l = 0, r = n-1;
+        while (l <= r) {
+            int mid = (l + r)>>1;
+            if(arr[mid] >= arr[0]) l = mid+1;
+            else r = mid-1;
+        }
+        return l;
+    }
+    
+   int bs(int l, int r, int val, vector<int> &arr){
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (arr[mid] == val) return mid;
+            if (arr[mid] > val) r = mid - 1;
+            else l = mid + 1;
+        }
+        return -1;
+    }
+    
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int pivot = findPivot(nums);
+        cout<<pivot<<endl;
+        if(pivot == n || target > nums[n-1]) return bs(0, pivot-1, target, nums); 
+        return bs(pivot, n-1, target, nums);  
+    }
+};
