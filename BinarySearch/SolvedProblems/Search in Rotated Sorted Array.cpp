da8d750802a423b4d7fd622e47ece46473a7beb3
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-06-20 17:17:35 +0530

    Update Search in Rotated Sorted Array.cpp

--- BinarySearch/SolvedProblems/Search in Rotated Sorted Array.cpp
+++ BinarySearch/SolvedProblems/Search in Rotated Sorted Array.cpp
@@ -7,7 +7,7 @@ class Solution {
             if(arr[mid] >= arr[0]) l = mid+1;
             else r = mid-1;
         }
-        return l;
+        return l == n ? 0 : l;
     }
     
    int bs(int l, int r, int val, vector<int> &arr){
