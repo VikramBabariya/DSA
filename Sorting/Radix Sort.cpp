4d6a135ac08242ff8b648ed70830ad700fbb0f33
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-07-01 09:53:31 +0530

    Update Radix Sort.cpp

--- Sorting/Radix Sort.cpp
+++ Sorting/Radix Sort.cpp
@@ -1,26 +1,21 @@
-void run_case() {
-	int arr[] = {237, 146, 259, 348, 152, 152, 163, 235, 48, 36, 62};
-
-	int n = 11;
-	vector<queue<int>> bucks(10);
-	int pass = ceil(log10(*max_element(arr, arr + n)));
-	for (int p = 0, div = 1; p < pass; p++, div *= 10) {
-		for (int i = 0; i < n; i++) {
-			int digit = (arr[i] / div) % 10;
-			bucks[digit].push(arr[i]);
-		}
-
-		int it = 0;
-		for (int i = 0; i < 10; i++) {
-			while (!bucks[i].empty()) {
-				arr[it] = bucks[i].front();
-				it++;
-				bucks[i].pop();
-			}
-		}
-	}
-
-	for (int i = 0; i < n; i++) {
-		cout << arr[i] << " ";
-	}
-}
+vector<int> radixSort(vector<int>arr, int n){
+        vector<queue<int>> bucks(10);
+    	int pass = floor(log10(*max_element(arr.begin(), arr.end()))) + 1;
+    	
+    	for (int p = 0, div = 1; p < pass; p++, div *= 10) {
+    		for (int i = 0; i < n; i++) {
+    			int digit = (arr[i] / div) % 10;
+    			bucks[digit].push(arr[i]);
+    		}
+    
+    		int it = 0;
+    		for (int i = 0; i < 10; i++) {
+    			while (!bucks[i].empty()) {
+    				arr[it] = bucks[i].front();
+    				it++;
+    				bucks[i].pop();
+    			}
+    		}
+    	}
+        return arr;
+    }
