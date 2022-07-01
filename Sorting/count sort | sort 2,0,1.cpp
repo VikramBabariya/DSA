82be347b77332ee31505c4318523734418906469
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-07-01 17:07:57 +0530

    Update count sort | sort 2,0,1.cpp

--- Sorting/count sort | sort 2,0,1.cpp
+++ Sorting/count sort | sort 2,0,1.cpp
@@ -1,16 +1,31 @@
-class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-        vector<int> cnt(3, 0);
-        int n = nums.size();
-        
-        for(int i = 0; i < n; i++) cnt[nums[i]]++;
-        
-        int it = 0; 
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < cnt[i]; j++, it++){
-                nums[it] = i;
-            }
+
+void sortColors(vector<int>& nums) {
+    vector<int> cnt(3, 0);
+    int n = nums.size();
+
+    for(int i = 0; i < n; i++) cnt[nums[i]]++;
+
+    int it = 0; 
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < cnt[i]; j++, it++){
+            nums[it] = i;
         }
     }
-};
+}
+
+
+void sortColors(vector<int>& nums) {
+    int low = 0, mid = 0, high = nums.size() - 1;
+
+    while(mid <= high)
+    {
+        switch(nums[mid])
+        {
+            case 0: swap(nums[low++], nums[mid++]); break;
+
+            case 1: mid++; break;
+
+            case 2: swap(nums[mid], nums[high--]); break;
+        }
+    }
+}
