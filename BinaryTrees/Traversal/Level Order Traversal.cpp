dacb451b43b56c22c5aa981c33a699aa39567e96
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-07-06 20:23:28 +0530

    Create Level Order Traversal.cpp

---
+++ BinaryTrees/Traversal/Level Order Traversal.cpp
@@ -0,0 +1,20 @@
+vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> ans;
+    if(root == NULL) return ans;
+
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()){
+        vector<int> level;
+        int qsize = q.size();
+        for(int i = 0; i < qsize; i++){ 
+            TreeNode *node = q.front();
+            q.pop();
+            if(node->left != NULL) q.push(node->left);
+            if(node->right != NULL) q.push(node->right);
+            level.push_back(node->val);
+        }
+        ans.push_back(level);
+    }
+    return ans;
+}
