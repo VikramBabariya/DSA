c9b4fbe767e220f3d7e8ace67a4adc931d881c6d
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-07-08 20:31:25 +0530

    Update Postorder Traversal.cpp

--- BinaryTrees/Traversal/Postorder Traversal.cpp
+++ BinaryTrees/Traversal/Postorder Traversal.cpp
@@ -14,3 +14,23 @@ vector<int> postorderTraversal(TreeNode* root) {
 }
 
 // iterative way
+vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> posttrv;
+    if(root == NULL) return posttrv;
+    stack<TreeNode*> st1;
+    st1.push(root);
+    TreeNode *node;
+
+    while(!st1.empty()){
+        node = st1.top();
+        st1.pop();
+        posttrv.push_back(node->val);
+        if(node->left != NULL) st1.push(node->left);
+        if(node->right != NULL) st1.push(node->right);
+    }
+    reverse(begin(posttrv), end(posttrv));
+    return posttrv;
+}
+
+// using one stack
+
