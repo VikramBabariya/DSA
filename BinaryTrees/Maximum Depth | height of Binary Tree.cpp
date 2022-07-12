42b0a207a125d92945a671ff45dffaab4b83fa3a
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-07-12 20:06:54 +0530

    Update Maximum Depth | height of Binary Tree.cpp

--- BinaryTrees/Maximum Depth | height of Binary Tree.cpp
+++ BinaryTrees/Maximum Depth | height of Binary Tree.cpp
@@ -7,3 +7,23 @@ int maxDepth(TreeNode* root) {
     int rd = maxDepth(root->right);
     return 1 + max(ld, rd);
 }
+
+// level order way
+int maxDepth(TreeNode* root) {
+    int maxdep = 0;
+    if(root == NULL) return maxdep;
+
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()){
+        int qsize = q.size();
+        for(int i = 0; i < qsize; i++){ 
+            TreeNode *node = q.front();
+            q.pop();
+            if(node->left != NULL) q.push(node->left);
+            if(node->right != NULL) q.push(node->right);
+        }
+        maxdep++;
+    }
+    return maxdep;
+}
