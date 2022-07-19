761d3cb56b6003fb134a68132d9569c76ceb7a07
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-07-19 08:50:59 +0530

    Update Top View Of BT.cpp

--- BinaryTrees/View/Top View Of BT.cpp
+++ BinaryTrees/View/Top View Of BT.cpp
@@ -1,52 +1,24 @@
-vector<vector<pair<int, int>>> UpdatedLevelOrder(TreeNode<int> * root, int &minVer, int &maxVer) {
-    vector<vector<pair<int, int>>> ans;
-    if(root == NULL) return ans;
-
-    queue<pair<TreeNode<int> *, int>> q; // node, ver
-    q.push({root, 0});
-    while(!q.empty()){
-        vector<pair<int, int>> level;
-        int qsize = q.size();
-        for(int i = 0; i < qsize; i++){ 
-            auto it = q.front();
-            TreeNode<int> *node = it.first;
-            int ver = it.second;
-            minVer = min(minVer, ver);
-            maxVer = max(maxVer, ver);
-            q.pop();
-            if(node->left != NULL) q.push({node->left, ver-1});
-            if(node->right != NULL) q.push({node->right, ver+1});
-            level.push_back({node->val, ver});
-        }
-        ans.push_back(level);
-    }
-
-    return ans;
-}
-
-vector<vector<int>> verticalTraversal(TreeNode<int> * root) {
-    int minVer = INT_MAX, maxVer = INT_MIN;
-    vector<vector<pair<int, int>>> lot = UpdatedLevelOrder(root, minVer, maxVer);
-    
-    int n = abs(minVer) + maxVer + 1;
-    vector<vector<int>> ans(n);
-
-    for(int i = 0; i < lot.size(); i++){
-        for(int j = 0; j < lot[i].size(); j++){
-            int t = lot[i][j].second + abs(minVer);
-            ans[t].push_back(lot[i][j].first);
-        }
-    }
-
-    return ans;
-}
-/*****************/ //updated part
 vector<int> getTopView(TreeNode<int> *root) {
-    vector<vector<int>> vot = verticalTraversal(root);
     vector<int> ans;
+    if(root == NULL) return ans;
     
-    for(auto ver : vot){
-        ans.push_back(ver[0]);
+    map<int, int> mp; // vertical - node value
+    queue<pair<TreeNode<int> *, int>> q; // node - vertical
+    q.push({root, 0});
+    
+    while(!q.empty()){
+        auto it = q.front();
+        TreeNode<int> * node = it.first;
+        int ver = it.second;
+        q.pop();
+        
+        if(!mp.count(ver)) mp[ver] = node->val;
+        
+        if(node->left) q.push({node->left, ver-1});
+        if(node->right) q.push({node->right, ver+1});
     }
-    return ans;  
+    for(auto &it : mp){
+        ans.push_back(it.second);
+    }
+    return ans;
 }
