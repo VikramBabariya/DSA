4eac67879fc52ebae21e46ae2f1954c40e88930f
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-07-19 21:31:07 +0530

    Create Lowest Common Ancestor(LCA).cpp

---
+++ BinaryTrees/Lowest Common Ancestor(LCA).cpp
@@ -0,0 +1,10 @@
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if(root == NULL || root == p || root == q) return root;
+
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+    if(left == NULL) return right;
+    if(right == NULL) return left;
+    return root;
+}
