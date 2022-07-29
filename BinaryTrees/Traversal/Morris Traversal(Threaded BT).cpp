c5e2303e21cf943e9a9e1580852084f624b17491
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-07-29 20:26:14 +0530

    Create Morris Traversal(Threaded BT).cpp

---
+++ BinaryTrees/Traversal/Morris Traversal(Threaded BT).cpp
@@ -0,0 +1,27 @@
+vector < int > inorderTraversal(node * root) {
+  vector < int > inorder;
+
+  node * cur = root;
+  while (cur != NULL) {
+    if (cur -> left == NULL) {
+      inorder.push_back(cur -> data);
+      cur = cur -> right;
+    } else {
+      node * prev = cur -> left;
+      while (prev -> right != NULL && prev -> right != cur) {
+        prev = prev -> right;
+      }
+
+      if (prev -> right == NULL) {
+        prev -> right = cur; 
+        //inorder.push_back(cur -> data); //for preorder traversal
+        cur = cur -> left;
+      } else {
+        prev -> right = NULL;
+        inorder.push_back(cur -> data);
+        cur = cur -> right;
+      }
+    }
+  }
+  return inorder;
+}
