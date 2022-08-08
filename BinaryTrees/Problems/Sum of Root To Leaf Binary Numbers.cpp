cf044b3e7c9a1459f6e512a98b3a6191a2cdbdb6
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-08-08 22:26:14 +0530

    Create Sum of Root To Leaf Binary Numbers.cpp

---
+++ BinaryTrees/Problems/Sum of Root To Leaf Binary Numbers.cpp
@@ -0,0 +1,35 @@
+// O(N^2)
+int BinToInt(vector<bool> &bin){
+    int res = 0;
+    for(int i = bin.size()-1, cnt = 0; i >= 0 ; i--, cnt++){
+        res += bin[i] * pow(2, cnt);
+    }
+    return res;
+}
+
+void sumBT(TreeNode* root, vector<bool> &path, int &sum){
+    if(root == NULL) return;
+
+    path.push_back(root->val);
+    sumBT(root->left, path, sum);
+    sumBT(root->right, path, sum);
+
+    if(root->left == NULL && root->right == NULL){
+        sum += BinToInt(path);
+    }
+    path.pop_back();
+}
+
+int sumRootToLeaf(TreeNode* root) {
+    int sum = 0;
+    vector<bool> path;
+    sumBT(root, path, sum);
+    return sum;
+}
+
+// O(N)
+int sumRootToLeaf(TreeNode* root, int val = 0) {
+    if(root == NULL) return 0;
+    val = 2 * val + root->val;
+    return (root->left == NULL && root->right == NULL) ? val : sumRootToLeaf(root->left, val) + sumRootToLeaf(root->right, val);
+}
