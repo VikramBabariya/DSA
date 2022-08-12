58ee0b1389a9b1fa02aee7a5d493d6fa043afed8
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-08-12 10:47:10 +0530

    Update Populating Next Right Pointers in Each Node.cpp

--- BinaryTrees/Problems/medium/Populating Next Right Pointers in Each Node.cpp
+++ BinaryTrees/Problems/medium/Populating Next Right Pointers in Each Node.cpp
@@ -1,18 +1,16 @@
 // O(NLogN) SC: O(Logn)
 //recursive
 Node* connect(Node* root) {
-    if(root == NULL) return root;
-    if(root->left == NULL && root->right == NULL) return root;
-
-    (root->left)->next = root->right;
-    Node *temp1 = root->left, *temp2 = root->right;
-    while(temp1->right && temp2->left){
-        temp1 = temp1->right; temp2 = temp2->left;
-        temp1->next = temp2;
+    if (!root) return NULL;
+    
+    if (root -> left) {
+        root -> left -> next = root -> right;
+        if (root -> next) {
+            root -> right -> next = root -> next -> left;
+        }
+        connect(root -> left);
+        connect(root -> right);
     }
-
-    connect(root->left);
-    connect(root->right);
     return root;
 }
 
