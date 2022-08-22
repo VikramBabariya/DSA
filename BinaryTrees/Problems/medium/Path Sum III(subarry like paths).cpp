fc02bfbabfed0cb306f68c0b8efbd7db1f3f2ff4
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-08-22 17:21:54 +0530

    Update Path Sum III(subarry like paths).cpp

--- BinaryTrees/Problems/medium/Path Sum III(subarry like paths).cpp
+++ BinaryTrees/Problems/medium/Path Sum III(subarry like paths).cpp
@@ -20,3 +20,16 @@ int pathSum(TreeNode* root, int targetSum) {
     dfs(root, targetSum, 0);
     return res;
 }
+
+
+// simple dfs way without using map
+int pathSum(TreeNode* root, int sum) {
+    if(!root) return 0;
+    return sumUp(root, 0, sum) + pathSum(root->left, sum) + pathSum(root->right, sum);
+}
+
+int sumUp(TreeNode* root, int pre, int& sum){
+    if(!root) return 0;
+    int current = pre + root->val;
+    return (current == sum) + sumUp(root->left, current, sum) + sumUp(root->right, current, sum);
+}
