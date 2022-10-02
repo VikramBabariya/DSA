2852e94f7e8acde7c51aa7609c1b4d730b9e49ca
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-10-02 12:01:25 +0530

    Update dfs.cpp

--- Graphs/Traversal/dfs.cpp
+++ Graphs/Traversal/dfs.cpp
@@ -1,45 +1,20 @@
-// Method 1 using stack
-
-vector<int> dfs(vector<vector<int>> adj, int n) {
-	vector<int> dfsTrv;
-	vector<bool> vis(n + 1, false);
-
-	for (int i = 1; i <= n; i++) {
-		if (vis[i] == false) {
-
-			stack<int> nodes;
-			nodes.push(i);
-			vis[i] = true;
-
-			while (!nodes.empty()) {
-				int node = nodes.top();
-				nodes.pop();
-				dfsTrv.push_back(node);
-
-				for (auto adjNode : adj[node]) {
-					if (vis[adjNode] == false) {
-						nodes.push(adjNode);
-						vis[adjNode] = true;
-					}
-				}
-			}
-
-		}
-	}
-	return dfsTrv;
-}
-
-
-// method 2 using recursion
 void dfsHelp(int node, vector<vector<int>> &adj, vector<bool> &vis, vector<int> &dfsTrv) {
 	vis[node] = true;
 	dfsTrv.push_back(node);
-
-	for (auto adjNode : adj[node]) {
-		if (vis[adjNode] == false) {
-			dfsHelp(adjNode, adj, vis, dfsTrv);
-		}
-	}
+	
+	// for adjMatrix
+	for (int i = 1; i < adj.size(); i++) {
+            if (adj[node][i] == 1 && vis[i] == false) {
+                dfsHelp(i, adj, vis);
+            }
+        }
+	
+	// for adjList
+// 	for (auto adjNode : adj[node]) {
+// 		if (vis[adjNode] == false) {
+// 			dfsHelp(adjNode, adj, vis, dfsTrv);
+// 		}
+// 	}
 }
 
 vector<int> dfs(vector<vector<int>> adj, int n) {
