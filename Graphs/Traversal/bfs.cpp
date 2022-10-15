1471cefc75661ba03e165deae7e79272090943a3
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-10-15 07:56:49 +0530

    Update bfs.cpp

--- Graphs/Traversal/bfs.cpp
+++ Graphs/Traversal/bfs.cpp
@@ -1,5 +1,5 @@
 // 1 based indexing
-vector<int> bfs(vector<vector<int>> adj, int n) {
+void bfs(vector<vector<int>> adj, int n) {
 // 	vector<int> bfsTrv;
 	vector<bool> vis(n + 1, false);
 	
@@ -14,24 +14,26 @@ vector<int> bfs(vector<vector<int>> adj, int n) {
 				int node = nodes.front();
 				nodes.pop();
 // 				bfsTrv.push_back(node);
-			
-				for(int i = 1; i < adj.size(); i++){
-					if (adj[node][i] == 1 && vis[i] == false) {
-						nodes.push(i);
-						vis[i] = true;
-					}
-				}
 				
-				//for adjList
-// 				for (auto adjNode : adj[node]) {
-// 					if (vis[adjNode] == false) {
-// 						nodes.push(adjNode);
-// 						vis[adjNode] = true;
-// 					}
-// 				}
+				for (auto adjNode : adj[node]) {
+ 					if (vis[adjNode] == false) {
+ 						nodes.push(adjNode);
+ 						vis[adjNode] = true;
+ 					}
+ 				}		
+				
+ 				
 			}
 
 		}
 	}
-	return bfsTrv;
+// 	return bfsTrv;
+}
+
+//for adjMatrix
+for(int i = 1; i < adj.size(); i++){
+	if (adj[node][i] == 1 && vis[i] == false) {
+		nodes.push(i);
+		vis[i] = true;
+	}
 }
