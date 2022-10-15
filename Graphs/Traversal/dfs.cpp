be65d1f948ec1ef9595567d72617d44b9acfdaca
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-10-15 22:09:18 +0530

    Update dfs.cpp

--- Graphs/Traversal/dfs.cpp
+++ Graphs/Traversal/dfs.cpp
@@ -1,28 +1,28 @@
 //1 based indexing
 
-void dfs(int node, vector<vector<int>> &adj, vector<bool> &vis) {
-	vis[node] = 1;
+void dfs(int node, vector<vector<int>> &adj, vector<bool> &state) {
+	state[node] = 1;
 	
 	for (auto adjNode : adj[node]) {
-		if (vis[adjNode] == 0) {
-			dfs(adjNode, adj, vis, dfsTrv);
+		if (state[adjNode] == 0) {
+			dfs(adjNode, adj, state);
 		}
 	}
 }
 
 // for adjMatrix
 for (int i = 1; i < adj.size(); i++) {
-    if (adj[node][i] == 1 && vis[i] == 0) {
-	dfsHelp(i, adj, vis);
+    if (adj[node][i] == 1 && state[i] == 0) {
+	dfsHelp(i, adj, state);
     }
 }
 
 void dfs(vector<vector<int>> adj, int n) {
-	vector<bool> vis(n + 1, 0);
+	vector<bool> state(n + 1, 0);
 
 	for (int i = 1; i <= n; i++) {
-		if (vis[i] == 0) {
-			dfs(i, adj, vis);
+		if (state[i] == 0) {
+			dfs(i, adj, state);
 		}
 	}
 }
