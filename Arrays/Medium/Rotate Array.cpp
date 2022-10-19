554702f73f733fd776f074f2fc629b928ee7684c
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-10-19 15:17:55 +0530

    Update Rotate Array.cpp

--- Arrays/Medium/Rotate Array.cpp
+++ Arrays/Medium/Rotate Array.cpp
@@ -13,3 +13,28 @@ void rotate(vector<int>& nums, int k) {
         nums[j] = temp[i];
     }
 }
+
+// sol2 O(1) extra space
+void reverse(vector<int>& nums, int i, int j){
+    int tmp = 0;       
+    while(i < j){
+        tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+        i++;
+        j--;
+    }
+}
+
+
+void rotate(vector<int>& nums, int k) {
+    int n = nums.size();
+    if(n < 2){
+        return;
+    }
+
+    k = k % n;
+    reverse(nums, 0, n - k - 1);
+    reverse(nums, n - k, n - 1);
+    reverse(nums, 0, n - 1);
+}
