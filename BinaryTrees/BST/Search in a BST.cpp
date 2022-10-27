4df2a8ac4388bf2bff6cfc92696c8158e9e525ff
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2022-10-27 15:12:52 +0530

    Create Search in a BST.cpp

---
+++ BinaryTrees/BST/Search in a BST.cpp
@@ -0,0 +1,8 @@
+//TC: O(height) ~ O(logN)
+
+TreeNode* searchBST(TreeNode* root, int val) {
+    if(root == NULL) return NULL;
+    if(root->val == val) return root;
+    if(root->val < val) return searchBST(root->right, val);
+    else return searchBST(root->left, val);
+}
