d76e0d2ca2a845eb558ffc1d1ec8e1118f350435
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2023-01-08 15:16:15 +0530

    Create Finding the Kth Prime.cpp

---
+++ Maths/Primes/Finding the Kth Prime.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int maxN = 86028121;
+vector<bool> isPrime(maxN + 1, true);
+vector<int> primes;
+
+void sieve() {
+	isPrime[0] = isPrime[1] = false;
+	for (int i = 2; i * i <= maxN; i++) {
+		if (isPrime[i] == true) {
+			for (int j = i * i; j <= maxN; j += i) {
+				isPrime[j] = false;
+			}
+		}
+	}
+	for (int i = 2; i <= maxN; i++) {
+		if (isPrime[i]) primes.push_back(i);
+	}
+
+}
+
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+	sieve();
+	int q;
+	cin >> q;
+	while (q--) {
+		int k;
+		cin >> k;
+		cout << primes[k - 1] << endl;
+	}
+
+	return 0;
+}
+
+
