ac95b74194d60585b14ec2d6ce3559ea4b3765aa
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2023-08-31 09:53:55 +0530

    Update  Sieve.cpp

--- Maths/Sieve/ Sieve.cpp
+++ Maths/Sieve/ Sieve.cpp
@@ -3,7 +3,7 @@
 int maxN = (int)1e7;
 vector<bool> isPrime(maxN+1, true);
 
-// TC: O(N * loglogN)
+// TC: O(N * logN)
 void sieve(){
     isPrime[0] = isPrime[1] = false;
     for(int i = 2; i<= maxN; i++){
@@ -29,8 +29,8 @@ void sieve(){
 
 
 
---------------------
-Finding Lowest and Highest prime divisor of a numbers
+//--------------------
+//Finding Lowest and Highest prime divisor of a numbers
 
 void low_high_prime_fact(int n){
     vector<bool> isPrime(n+1, true);
@@ -44,6 +44,38 @@ void low_high_prime_fact(int n){
                 hp[j] = i;
                 if(lp[j] == 0) lp[j] = i;
             }
+        }
+    }
+}
+
+
+
+//-------------------------
+//Prime Factorization of a num in O(logN)
+
+void prime_fact(int num){
+    vector<int> pfact;
+    while(num > 1){
+        int hpf = hp[num]; // we need to find lowest or highest prime factors of all nums using sieve method first
+        while(num % hpf == 0){
+            pfact.push_back(hpf);
+            num /= hpf;
+        }
+    }
+    
+}
+
+
+//---------------------
+//Finding Divisors of all nums upto n  -- O(NlogN)
+
+void divisors(int n){
+    vector<int> divs[n+1];
+    int sumDiv[n+1]; // sum of divisors
+    for(int i = 2; i <= n; i++){
+        for(int j = i; j <= n; j += i){
+            divs[j].push_back(i);
+            sumDiv[j] += i;
         }
     }
 }
