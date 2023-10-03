e0e7b93dcd296d410abff193a06e2f82bb3353e9
VikramBabariya <83386820+VikramBabariya@users.noreply.github.com>
2023-10-03 19:12:49 +0530

    Update and rename Bit Manipulation/Lerning Problem/Subset Of A Given Set.cpp to Bit Manipulation/Power Set.cpp

--- Bit Manipulation/Power Set.cpp
+++ Bit Manipulation/Power Set.cpp
@@ -1,12 +1,9 @@
-vector<vector<int>> subsets(vector<int>& nums) {
-    vector<vector<int>> ans;
+void subsets(vector<int>& nums) {
     int n = nums.size(), total = pow(2, n);
     for(int num = 0; num < total; num++){
         vector<int> ss;
         for(int i = 0; i < 32; i++){
             if(num & 1 << i) ss.push_back(nums[i]);
         }
-        ans.push_back(ss);
     }
-    return ans;
 }
